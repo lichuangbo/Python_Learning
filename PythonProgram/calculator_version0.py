0694dbf1bbc75592589a3436b4016f55eed839b0
lichuangbo <15503609389@163.com>
2019-03-29 06:18:20 -0500

    calculator version1 10位数以内的+-*/()运算

--- PythonProgram/calculator_version0.py
+++ PythonProgram/calculator_version0.py
@@ -1,7 +1,3 @@
-cal_str = input('输入算式：')
-cal_str_rep = cal_str.replace(' ', '')
-
-
 def is_operator(ch):
     '''
     是否为运算符
@@ -22,13 +18,20 @@ def getStackPeek(stack):
         return stack[len(stack) - 1]
 
 
+def pop(stack):
+    if len(stack) == 0:
+        return 0
+    else:
+        return stack.pop()
+
+
 def getprior(ch):
     '''
     取得operator的优先级
     :param ch: operator
     :return: 对应优先级
     '''
-    operator_prio = {'(': 5, '*': 4, '/': 3, '+': 2, '-': 1, ')': 5, '=': 0}
+    operator_prio = {'(': 3, '*': 2, '/': 2, '+': 1, '-': 1, ')': 3, '=': 0}
     if ch in operator_prio:
         return operator_prio[ch]
     else:
@@ -55,22 +58,29 @@ def calculate(a, b, operator):
     return result
 
 
-def instack(str):
+def cal(str):
     '''
-    1 + 2 * 3 =
     数字：
         入数字栈
     非数字(运算符)：
         如果运算符栈空：
             入栈
-        如果优先级 > 栈顶优先级：
-            入栈
-        如果优先级 < 栈顶优先级：
-            数字栈弹出俩数，运算符栈弹出栈顶；计算结果放入数字栈
-        如果优先级 = 栈顶优先级：
-            ()的情况，运算符栈弹栈顶，跳过此次循环读下一字符
-    :param str:
-    :return:
+        如果栈顶运算符(：
+		    如果当前运算符)：
+		        弹栈
+			否则:
+				不用比较当前运算符直接入栈
+        否则：
+            如果优先级 > 栈顶优先级：
+                ）：计算并压栈
+                其他：弹栈
+            如果优先级 < 栈顶优先级：
+                数字栈弹出俩数，运算符栈弹出栈顶；计算结果放入数字栈
+            如果优先级 = 栈顶优先级：
+                ():弹栈
+                +- or */: 计算并压栈
+    :param str: string expression
+    :return: result
     '''
     num_stack = []
     oper_stack = []
@@ -79,28 +89,53 @@ def instack(str):
         if not is_oper:
             temp = int(e)
             num_stack.append(temp)
-            continue
         else:
             while True:
                 if len(oper_stack) == 0:
                     oper_stack.append(e)
+                if getStackPeek(oper_stack) == '(':
+                    if e == ')':
+                        oper_stack.pop()
+                    else:
+                        oper_stack.append(e)
                     break
-                if getprior(getStackPeek(oper_stack)) < getprior(e):
-                    oper_stack.append(e)
-                    break
-                elif getprior(getStackPeek(oper_stack)) > getprior(e):
-                    num2 = num_stack.pop()
-                    num1 = num_stack.pop()
-                    op = oper_stack.pop()
-                    num_stack.append(calculate(num1, num2, op))
                 else:
-                    oper_stack.pop()
-                    break
-    if getStackPeek(oper_stack) == '=':
-        return getStackPeek(num_stack)
-    # return num_stack, oper_stack
+                    if getprior(getStackPeek(oper_stack)) < getprior(e):
+                        if e == ')':
+                            num2 = pop(num_stack)
+                            num1 = pop(num_stack)
+                            op = pop(oper_stack)
+                            num_stack.append(calculate(num1, num2, op))
+                        else:
+                            oper_stack.append(e)
+                            break
+                    elif getprior(getStackPeek(oper_stack)) > getprior(e):
+                        num2 = pop(num_stack)
+                        num1 = pop(num_stack)
+                        op = pop(oper_stack)
+                        num_stack.append(calculate(num1, num2, op))
+                    else:
+                        if getStackPeek(oper_stack) == '(':
+                            oper_stack.pop()
+                            break
+                        else:
+                            num2 = pop(num_stack)
+                            num1 = pop(num_stack)
+                            op = pop(oper_stack)
+                            num_stack.append(calculate(num1, num2, op))
+    result = getStackPeek(num_stack)
+    num_stack.clear()
+    oper_stack.clear()
+    return result
 
 
-print(instack(cal_str_rep))
-
-
+if __name__ == '__main__':
+    cal_str = input('输入算式：')
+    cal_str_rep = cal_str.replace(' ', '')
+    print(cal(cal_str_rep))
+'''
+测试数据：
+3 * (6 - 2 * 2)= 6
+(6-3 +(4/5) * (9-2*5/3 + 6 /3*9/4*2 +3 * 5/1 )) - (4*3)/ (6-3*1)= 22.733333333333334
+6-3+4/5= 3.8
+'''
